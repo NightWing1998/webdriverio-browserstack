23b1b4dce087c47f976233064d3cea98318ada0b
Samiran Saha <samiran.s89@gmail.com>
2020-04-29 21:59:59 +0530

    local test changes

--- tests/specs/local_test.js
+++ tests/specs/local_test.js
@@ -1,12 +1,21 @@
 var common = require('./helper/common');
 var sessionId;
-var globalResult = 'complete';
+var globalStatus = 'complete';
+var actualResult, expectedResult = "Local Server"
+var local_website_test = 'can test local websites'
 
-describe('BrowserStack Local Testing', function() {
+describe('Local Website Testing on BrowserStack', function() {
   it('can check tunnel working', function () {
     browser.url('http://bs-local.com:45691/check')
     browser.getPageSource().should.match(/Up and running/i);
   });
+
+  it(local_website_test, function () {
+    browser.url('http://bs-local.com:8000')
+    actualResult = browser.getTitle()
+    actualResult.should.equal(expectedResult);
+  });
+
 
   it('test status can be marked on BrowserStack', () => {
     // this is just a dummy test in the same session (same suite) with a delay so that the afterEach hook executes.
@@ -17,9 +26,15 @@ describe('BrowserStack Local Testing', function() {
 
   afterEach(function(done){
     const finalTestStatus = this.currentTest.state
-    if((finalTestStatus === 'passed' && globalResult === 'complete') || (finalTestStatus === 'failed') ) {
-      common.mark_test_status(browser.sessionId, finalTestStatus , "N/A", this.currentTest.parent.title)
-      globalResult = finalTestStatus
+    if(this.currentTest.title === local_website_test) {
+      if(finalTestStatus === 'passed') {
+        common.mark_test_status(browser.sessionId, finalTestStatus , "Success: expected '"+ expectedResult+ "'" + " matches '" + actualResult + "'" , this.currentTest.parent.title)
+      }
+      else {
+        common.mark_test_status(browser.sessionId, finalTestStatus , "expected '"+ expectedResult+ "'" + " to equal '" + actualResult + "'", this.currentTest.parent.title)
+      }
+
+      globalStatus = finalTestStatus
     }
     });
 });
